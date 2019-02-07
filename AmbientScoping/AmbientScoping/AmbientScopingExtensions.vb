425ef7bd5e4eb1de706ced513a65674862c6e0b3
Tobias Korn <email@tobiaskorn.de>
2019-02-07 16:10:00 +0100

    initial Commit

---
+++ AmbientScoping/AmbientScoping/AmbientScopingExtensions.vb
@@ -0,0 +1,27 @@
+﻿Imports System
+Imports System.Collections.Generic
+
+Public Module AmbientScopingExtensions
+
+  Private _Instances As New Dictionary(Of Type, Object)
+
+  Public Sub ApplyInstance(Of T)(ByRef target As T)
+    Dim targeType As Type = GetType(T)
+    SyncLock _Instances
+      If (_Instances.ContainsKey(targeType)) Then
+        target = DirectCast(_Instances(targeType), T)
+        Exit Sub
+      End If
+    End SyncLock
+    target = DirectCast(Activator.CreateInstance(targeType), T)
+    SyncLock _Instances
+      If (_Instances.ContainsKey(targeType)) Then
+        target = DirectCast(_Instances(targeType), T)
+      Else
+        _Instances.Add(targeType, target)
+      End If
+    End SyncLock
+
+  End Sub
+
+End Module
---
+++ Pack-AmbientScoping-Release.bat
@@ -0,0 +1,1 @@
+nuget pack ./AmbientScoping.nuspec -Build -Symbols -OutputDirectory "..\..\(NuGetRepo)" -InstallPackageToOutputPath
